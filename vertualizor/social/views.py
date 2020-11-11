5fa253ddcfa93d4fdfd785511d9b79faedc5abd3
ali <weplutus.1@gmail.com>
2020-11-11 16:18:37 +0300

    first create data from ./mamange shell then crea json data in views

--- vertualizor/social/views.py
+++ vertualizor/social/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+from .models import Tweet
 
 
 def home_view(request, *args, **kwards):
@@ -8,4 +9,11 @@ def home_view(request, *args, **kwards):
 
 
 def post_view(request, post_id, *args, **kwards):
-    return HttpResponse(f"<h1> the post id is {post_id} </h1>")
+    obj = Tweet.objects.get(id=post_id)
+    data = {
+        'id': post_id,
+        'content': obj.content,
+        # 'image_bath': obj.image.url #we will use this later
+    }
+    # HttpResponse(f"<h1> the post id is {post_id}, {obj.content} </h1>")
+    return JsonResponse(data)
