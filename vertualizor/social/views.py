d14b0bea12deca9119463fdcc365cb7fc447fe12
ali <weplutus.1@gmail.com>
2020-11-11 17:39:39 +0300

    add template, and create tempaltes and show them in views

--- vertualizor/social/views.py
+++ vertualizor/social/views.py
@@ -5,15 +5,23 @@ from .models import Tweet
 
 
 def home_view(request, *args, **kwards):
-    return HttpResponse("<h1> Hello django </h1>")
+    return render(request, 'pages/home.html', context={}, status=200)
 
 
 def post_view(request, post_id, *args, **kwards):
-    obj = Tweet.objects.get(id=post_id)
-    data = {
-        'id': post_id,
-        'content': obj.content,
-        # 'image_bath': obj.image.url #we will use this later
-    }
-    # HttpResponse(f"<h1> the post id is {post_id}, {obj.content} </h1>")
-    return JsonResponse(data)
+    try:
+        obj = Tweet.objects.get(id=post_id)
+        data = {
+            'id': post_id,
+            'content': obj.content,
+            # 'image_bath': obj.image.url  # we will use this later
+        }
+        status = 200
+    except:
+        data = {
+            'error': '404',
+            'content': 'no such post',
+            # 'image_bath': obj.image.url  # we will use this later
+        }
+        status = 404
+    return JsonResponse(data, status=status)
