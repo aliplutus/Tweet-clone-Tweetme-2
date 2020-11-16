867189b8dc2bc1cf60622da1822d80c19090ccd9
ali <weplutus.1@gmail.com>
2020-11-16 15:52:24 +0300

    1. reoderig in the reverse drection, 2. textarea is required, 3. render only if xhr.status =201, 4. update container.innerhtml (render only new posts insead of reloading all of them).

--- vertualizor/social/models.py
+++ vertualizor/social/models.py
@@ -6,6 +6,10 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     imagePath = models.FileField(upload_to='images/', blank=True, null=True)
 
+    class Meta:
+        # this reverse the order of tweets but I don't understand how.
+        ordering = ["-id"]
+
     def serialize(self):
         return {
             'id': self.id,
