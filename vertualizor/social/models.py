98d29fd1c10768e2f9fe38315b455088005c18d3
ali <weplutus.1@gmail.com>
2020-11-16 18:37:56 +0300

    associate each posts to a spesifc user.

--- vertualizor/social/models.py
+++ vertualizor/social/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 import random
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
+    # each post corsponded for a spesific user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, null=True,on_delete=models.SET_null) # top keep posts in case users deleted
     content = models.TextField(blank=True, null=True)
     imagePath = models.FileField(upload_to='images/', blank=True, null=True)
 
