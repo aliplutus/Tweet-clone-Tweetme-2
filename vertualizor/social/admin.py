5128b09e0b699a96df86c9fb857cc6014ee6aa8a
ali <weplutus.1@gmail.com>
2020-11-17 09:24:23 +0300

    search_fields and list_display

--- vertualizor/social/admin.py
+++ vertualizor/social/admin.py
@@ -1,5 +1,18 @@
+from .models import Tweet
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tweet
-admin.site.register(Tweet)
+
+
+class PostsAdmin(admin.ModelAdmin):
+    # where to search.
+    search_fields = ['content', "user__username", "user__email"]
+    # this to dispaly USER Content
+    # "__str__" dysplay object(id) by defualt
+    list_display = ["__str__", 'user', 'content']
+
+    class Meta:
+        model = Tweet
+
+
+admin.site.register(Tweet, PostsAdmin)
