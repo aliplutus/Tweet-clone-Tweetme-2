1cc4057b2d780eb4519991d5d815b0bbf95d1606
ali <weplutus.1@gmail.com>
2020-11-21 16:20:48 +0300

    u can also use self.user

--- vertualizor/social/tests.py
+++ vertualizor/social/tests.py
@@ -9,13 +9,13 @@ User = get_user_model()
 
 class TweetTestCase(TestCase):
     def setUp(self):
-        User.objects.create_user(
+        self.user = User.objects.create_user(
             username='IamANewUser', password='newPassWord')
         User.objects.create_user(
             username='IamANewUser2', password='newPassWord2')
 
     def test_user_create(self):
         user = User.objects.get(username='IamANewUser')
-        self.assertEqual(user.username, 'IamANewUser')
+        self.assertEqual(self.user.username, 'IamANewUser')
         user = User.objects.get(username='IamANewUser2')
         self.assertEqual(user.username, 'IamANewUser2')
