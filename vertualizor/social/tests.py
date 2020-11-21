f6f4a7e806aa65434cb013f213760aa5e8f6f3dd
ali <weplutus.1@gmail.com>
2020-11-21 16:17:54 +0300

    django Tests, Test_user_create

--- vertualizor/social/tests.py
+++ vertualizor/social/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+# run ./manage.py test to test.
+from django.contrib.auth import get_user_model
+from .models import Tweet
+User = get_user_model()
+
+
+class TweetTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(
+            username='IamANewUser', password='newPassWord')
+        User.objects.create_user(
+            username='IamANewUser2', password='newPassWord2')
+
+    def test_user_create(self):
+        user = User.objects.get(username='IamANewUser')
+        self.assertEqual(user.username, 'IamANewUser')
+        user = User.objects.get(username='IamANewUser2')
+        self.assertEqual(user.username, 'IamANewUser2')
